dad2be35c3088beaa963091a5aaf1418b8666f85
mrtarasyuk <37049372+mrtarasyuk@users.noreply.github.com>
2018-03-14 19:25:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $('document').ready(function() {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
+        $('body,html').animate({scrollTop: top}, 1050);
     });
 
 });
